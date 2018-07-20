988c679ddb847eac48beaf91b63704be6d202d38
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2018-07-20 20:07:31 +0000

    Set devise secret key from an environment variable

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,8 +8,8 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '9cc2c57ef3944dd982d5a3e638e57e5086198e55aa51f7103fe0c85d0d1c5f4e19a361331209eef8c42fd1088010a828312857a7d17b5b654393f4dc20beab89'
-  
+  config.secret_key = '<%= ENV["DEVISE_SECRET_KEY"] %>'
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
