bd904ff16a09f77ecc85cd816a51ccd2f6122808
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2018-07-25 14:52:40 +0000

    Implement User#make_admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def make_admin
+    update!(:admin => true)
+  end
 end
